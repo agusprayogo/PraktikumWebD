5a02fd6d77cd884107efb6c45e671da4638852a0
agusprayogo <56584996+agusprayogo@users.noreply.github.com>
2020-05-06 23:54:20 +0800

    Create index.php

---
+++ tugas 11/index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<table>
+		<tr>
+			<th>mahasiswa</th>
+		</tr>
+		<tr>
+			<td>
+				<form method="post">
+					<input type="text" name="namamahasiswa" placeholder="cari nama mahasiswa...">
+					<button type="submit">filter</button>
+				</form>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<form method="post">
+					urut berdasar:
+					<input type="radio" name="filmahasiswa" value="nim">NIM
+					<input type="radio" name="filmahasiswa" value="nama">nama
+					<input type="radio" name="filmahasiswa" value="alamat">alamat
+					<input type="radio" name="filmahasiswa"value="telepon">telepon
+					<input type="radio" name="filmahasiswa" value="status">status
+					<button type="submit">urut</button>
+				</form>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<table>
+					<tr>
+						<th>nim</th>
+						<th>nama</th>
+						<th>alamat</th>
+						<th>telepon</th>
+						<th>status</th>
+					</tr>
+					<?php
+		$conn = mysqli_connect('localhost','root','','sorfil');
+		$str='';
+		if (isset($_POST['namamahasiswa']) and $_POST['namamahasiswa']!='') {
+			$str=$str.' where nama like "%'.$_POST['namamahasiswa'].'%"';
+		}
+		if (isset($_POST['filmahasiswa'])) {
+			$str=$str.' order by '.$_POST['filmahasiswa'].'';
+		}
+		echo 'select * from mahasiswa'.$str;
+		$result = $conn->query('select * from mahasiswa'.$str);
+		if($result-> num_rows>0){
+			while ($row=$result-> fetch_assoc()) {
+				echo '<tr>';
+					echo '<td>';
+						echo '<label>'.$row['nim'].'</label>';
+					echo '</td>';
+					echo '<td>';
+						echo '<label>'.$row['nama'].'</label>';
+					echo '</td>';
+					echo '<td>';
+						echo '<label>'.$row['alamat'].'</label>';
+					echo '</td>';
+					echo '<td>';
+						echo '<label>'.$row['telepon'].'</label>';
+					echo '</td>';
+					echo '<td>';
+						echo '<label>'.$row['status'].'</label>';
+					echo '</td>';
+				echo '<tr>';
+			}
+		}
+	?>
+				</table>
+			</td>
+		</tr>
+		<tr>
+			<th>dosen</th>
+		</tr>
+		<tr>
+			<td>
+				<form method="post">
+					<input type="text" name="namadosen" placeholder="cari nama dosen...">
+					<button type="submit">filter</button>
+				</form>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<form method="post">
+					urut berdasar:
+					<input type="radio" name="fildosen" value="nip">NIP
+					<input type="radio" name="fildosen" value="nidn">NIDN
+					<input type="radio" name="fildosen" value="nama">nama
+					<input type="radio" name="fildosen" value="email">email
+					<button type="submit">urut</button>
+				</form>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<table>
+					<tr>
+						<th>NIP</th>
+						<th>NIDN</th>
+						<th>nama</th>
+						<th>email</th>
+					</tr>
+					<?php
+		$conn = mysqli_connect('localhost','root','','sorfil');
+		$str='';
+		if (isset($_POST['namadosen']) and $_POST['namadosen']!='') {
+			$str=$str.' where nama like "%'.$_POST['namadosen'].'%"';
+		}
+		if (isset($_POST['fildosen'])) {
+			$str=$str.' order by '.$_POST['fildosen'].'';
+		}
+		$result = $conn->query('select * from dosen'.$str);
+		if($result-> num_rows>0){
+			while ($row=$result-> fetch_assoc()) {
+				echo '<tr>';
+					echo '<td>';
+						echo '<label>'.$row['nip'].'</label>';
+					echo '</td>';
+					echo '<td>';
+						echo '<label>'.$row['nidn'].'</label>';
+					echo '</td>';
+					echo '<td>';
+						echo '<label>'.$row['nama'].'</label>';
+					echo '</td>';
+					echo '<td>';
+						echo '<label>'.$row['email'].'</label>';
+					echo '</td>';
+				echo '<tr>';
+			}
+		}
+	?>
+				</table>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
