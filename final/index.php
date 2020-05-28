5d1042f251253ebdeb552878d91cf675ba8f897b
agusprayogo <56584996+agusprayogo@users.noreply.github.com>
2020-05-28 19:45:59 +0800

    Create index.php

---
+++ final/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div align="center">
+		<h1>Menu</h1>
+		<table>
+			<tr>
+				<th>mahasiswa</th>
+				<th>dosen</th>
+			</tr>
+			<tr>
+				<td>
+					<a href="mahasiswa.php">daftar mahasiswa</a>
+				</td>
+				<td>
+					<a href="dosen.php">daftar dosen
+				</td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
