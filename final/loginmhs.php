55c56a2756003f120173f228435020155b072ad4
agusprayogo <56584996+agusprayogo@users.noreply.github.com>
2020-05-28 19:30:31 +0800

    Create loginmhs.php

---
+++ final/loginmhs.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+</head>
+<body>
+	<div align="center">
+		<h1>Login Mahasiswa</h1>
+		<form method="post" action="loginpros.php">
+			<input type="text" name="type" value="1" hidden>
+			<input type="text" name="nim" placeholder="NIM"><br><br>
+			<input type="password" name="password" placeholder="password"><br><br>
+			<button type="submit">login</button>
+		</form>
+	</div>
+</body>
+</html>
